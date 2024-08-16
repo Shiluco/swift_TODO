a33aeefa33d0160fbd59c719bd9177fee68f8d2e
Shiluco <hirap1939@gmail.com>
2024-08-16 14:33:35 +0900

    Initial commit

---
+++ TODO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TODO
+//
+//  Created by 佐藤 墾 on 2024/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by 佐藤 墾 on 2024/08/16.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
